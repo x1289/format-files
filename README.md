253070d717331d385c40a1e004ff8d0c57535c05
x1289 <daniel.pschibila@googlemail.com>
2022-04-04 20:55:28 +0200

    [Feature] Basic implementation

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # format-files
 Small script to format all files in a directory
+
+## Usage  
+
+### handleAllFiles(dirPath, formatFunc)  
+  
+dirPath:      Path to directory to format.  
+formatFunc:   Function to apply to every line of every file in the directory.  
+  
+
+  
+### formatFile(filePath, formatFunc)  
+  
+filePath:     File to format.  
+formatFunc:   Function to apply to every line of every file in the directory.  
+  
+  
+  
+
+### Example  
+
+#### To remove all trailing whitespaces and add uniform EOL character
+
+> await handleAllFiles('./testing/', (str) => {  
+>    return str.trimRight() + '\n';  
+>  });  
+
+#### To make every line uppercase
+
+> await handleAllFiles('./testing/', (str) => {  
+>    return str.toUpperCase() + '\n';  
+>  });  
---
+++ format-files.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
+
+function formatFile(filePath, formatFunc) {
+  return new Promise((resolve, reject) => {
+    if (typeof formatFunc !== 'function') reject(new Error('invalid format function.'));
+    fs.copyFile(filePath, filePath + '.temp', () => {
+      fs.writeFileSync(filePath, '');
+      const rl = readline.createInterface({
+        input: fs.createReadStream(filePath + '.temp'),
+        crlfDelay: Infinity
+      });
+      
+      rl.on('line', (line) => {
+        fs.appendFileSync(filePath, formatFunc(line));
+      });
+      
+      rl.on('close', () => {
+        rl.close();
+        fs.rmSync(filePath + '.temp', {force: true})
+        resolve(filePath);
+      });
+    })
+  })
+}
+
+function handleAllFiles(dirPath, formatFunc) {
+  return new Promise((resolve, reject) => {
+    fs.opendir(dirPath, async (err, dir) => {
+      if (err) reject(err);
+      for await (const dirent of dir) {
+        const newPath = path.join(dirPath, dirent.name);
+        if (dirent.isFile()) {
+          formatFile(newPath, formatFunc);
+        } else if (dirent.isDirectory()) {
+          handleAllFiles(newPath, formatFunc);
+        }
+      }
+    });
+  });
+}
+
+const x = async () => {
+  await handleAllFiles('./testing/', (str) => {
+    return str.trimRight() + '\n';
+  });
+}
+
+x();
